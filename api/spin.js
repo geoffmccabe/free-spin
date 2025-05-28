14044e28acd8f051ea5f1846d2b8913a1a5e59af
Geoff McCabe <geoffmccabe@gmail.com>
2025-05-28 15:26:31 -0600

    Update spin.js

--- api/spin.js
+++ api/spin.js
@@ -17,6 +17,13 @@ const connection = new Connection('https://api.mainnet-beta.solana.com');
 const supabase = createClient(SUPABASE_URL, SUPABASE_SERVICE_ROLE_KEY);
 
 export default async function handler(req, res) {
+  res.setHeader('Access-Control-Allow-Origin', 'https://solspin.lightningworks.io');
+  res.setHeader('Access-Control-Allow-Methods', 'POST, OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+
+  if (req.method === 'OPTIONS') {
+    return res.status(200).end();
+  }
   try {
     if (req.method !== 'POST') return res.status(405).json({ error: 'Method not allowed' });
 
